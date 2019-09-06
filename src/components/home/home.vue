47b9e1d3f4ab22ac4cdf7da41699b12dc140af01
liuchongchong <593261887@qq.com>
2019-09-07 02:04:56 +0800

    调整侧边导航栏

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -13,7 +13,65 @@
           </el-row>
         </el-header>
         <el-container>
-          <el-aside width="200px" class="aside">Aside</el-aside>
+          <el-aside width="200px" class="aside">
+
+            <el-menu
+              background-color="#393D49"
+              text-color="#fff"
+              :unique-opened="true"
+            >
+              <!--1-->
+              <el-submenu index="1">
+                <template slot="title">
+                  <i class="el-icon-location"></i>
+                  <span>导航一</span>
+                </template>
+                  <el-menu-item index="1-1">
+                      <i class="el-icon-location"></i>
+                      <span>选项1</span>
+                  </el-menu-item>
+                  <el-menu-item index="1-2">
+                    <i class="el-icon-location"></i>
+                    <span>选项2</span>
+                    </el-menu-item>
+              </el-submenu>
+              <!--2-->
+              <el-submenu index="2">
+                <template slot="title">
+                  <i class="el-icon-s-goods"></i>
+                  <span>导航二</span>
+                </template>
+                <el-menu-item index="2-1">
+                  <i class="el-icon-location"></i>
+                  <span>选项1</span>
+                </el-menu-item>
+              </el-submenu>
+              <!--3-->
+              <el-submenu index="3">
+                <template slot="title">
+                  <i class="el-icon-location"></i>
+                  <span>导航三</span>
+                </template>
+                <el-menu-item index="3-1">
+                  <i class="el-icon-location"></i>
+                  <span>选项1</span>
+                </el-menu-item>
+              </el-submenu>
+              <!--4-->
+              <el-submenu index="4">
+                <template slot="title">
+                  <i class="el-icon-location"></i>
+                  <span>导航四</span>
+                </template>
+                <el-menu-item index="4-1">
+                  <i class="el-icon-location"></i>
+                  <span>选项五</span>
+                </el-menu-item>
+              </el-submenu>
+              <!--5-->
+            </el-menu>
+
+          </el-aside>
           <el-main class="main">Main</el-main>
         </el-container>
       </el-container>
@@ -36,6 +94,9 @@
   .aside {
     background-color: #393D49;
     /*height: 100%;*/
+  }
+  .aside ul {
+    width: 200px;
   }
   .main {
     background-color: #e9eef3;
