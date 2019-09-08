685347ee310b0ed12f35508f007356053af8cb2a
liuchongchong <593261887@qq.com>
2019-09-08 17:43:38 +0800

    用户操作，添加修改

--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -11,10 +11,15 @@
       <el-col>
         <el-input placeholder="请输入内容"
                   v-model="input3"
+                  clearable
+                  @clear="delect()"
                   class="inputselect">
-          <el-button slot="append" icon="el-icon-search"></el-button>
+          <el-button
+            slot="append"
+            @click="searchUser"
+            icon="el-icon-search"></el-button>
         </el-input>
-        <el-button type="success">添加用户</el-button>
+        <el-button type="success" @click="showAddUser">添加用户</el-button>
       </el-col>
     </el-row>
   <!--表格-->
@@ -60,6 +65,7 @@
           <el-switch
             v-model="scope.row.value1"
             active-color="#13ce66"
+            @change="changeMgState(scope.row)"
             inactive-color="#ff4949">
           </el-switch>
         </template>
@@ -68,12 +74,71 @@
         prop="name"
         label="操作">
         <template slot-scope="scope">
-            <el-button size="mini" plain="true" type="primary" icon="el-icon-edit" circle></el-button>
-          <el-button size="mini" plain="true" type="danger" icon="el-icon-delete" circle></el-button>
-            <el-button size="mini" plain="true" type="success" icon="el-icon-check" circle></el-button>
+            <el-button size="mini"
+                       plain type="primary"
+                       icon="el-icon-edit"
+                       @click="edituser(scope.row)"
+                       circle></el-button>
+            <el-button size="mini"
+                       plain type="danger"
+                       icon="el-icon-delete"
+                       @click="delectuser(scope.row.id)"
+                       circle></el-button>
+            <el-button size="mini"
+                       plain type="success"
+                       icon="el-icon-check"
+                       circle></el-button>
         </template>
       </el-table-column>
     </el-table>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="pagenum"
+      :page-sizes="[1 ,2, 3, 4]"
+      :page-size="1"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total">
+    </el-pagination>
+  <!--添加用户对话框-->
+    <el-dialog title="添加用户" :visible.sync="dialogFormVisible">
+      <el-form :model="form">
+        <el-form-item label="用户名称" label-width="100px">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" label-width="100px">
+          <el-input v-model="form.passworld" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" label-width="100px">
+          <el-input v-model="form.emile" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" label-width="100px">
+          <el-input v-model="form.tel" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addUser()">确 定</el-button>
+      </div>
+    </el-dialog>
+  <!--编辑用户-->
+    <el-dialog title="编辑用户" :visible.sync="edit">
+      <el-form :model="form">
+        <el-form-item label="用户名称" label-width="100px">
+          <el-input v-model="form.name" disabled autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" label-width="100px">
+          <el-input v-model="form.emile" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" label-width="100px">
+          <el-input v-model="form.tel" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="edit = false">取 消</el-button>
+        <el-button type="primary" @click="editadduser()">确 定</el-button>
+      </div>
+    </el-dialog>
   </el-card>
 </template>
 
@@ -88,37 +153,50 @@
                     creattime: '2018-8-1',
                     name: '王小虎',
                     address: '上海市普陀区金沙江路 1518 弄',
-                    value1:true
+                    value1:true,
+                    id:455
                 }, {
                     creattime: '2018-8-2',
                     name: '王小虎',
                     address: '上海市普陀区金沙江路 1517 弄',
-                    value1:false
+                    value1:false,
+                    id:5555
                 }],
                 //分页相关的数据
-                total:0,
+                total:50,
                 pagenum:1,
-                pagesize:2
+                pagesize:2,
+            //添加对话框
+                form:{
+                    creattime:'',
+                    name:'',
+                    passworld:'',
+                    emile:'',
+                    tel:''
+                },
+                dialogFormVisible:false,
+                edit:false,
+                // curruserid:-1
             }
         },
         created(){
           // this.getuserList()
-            const res = {
-                'data':{
-                    'userslist':[{
-                        date: '2016-05-02',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1518 弄'
-                    }, {
-                        date: '2016-05-04',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1517 弄'
-                    }],
-                    'total':10
-                }
-            }
-            const {data:{userslist,total,code}} = res;
-            console.log(userslist.row);
+          //   const res = {
+          //       'data':{
+          //           'userslist':[{
+          //               date: '2016-05-02',
+          //               name: '王小虎',
+          //               address: '上海市普陀区金沙江路 1518 弄'
+          //           }, {
+          //               date: '2016-05-04',
+          //               name: '王小虎',
+          //               address: '上海市普陀区金沙江路 1517 弄'
+          //           }],
+          //           'total':10
+          //       }
+          //   }
+          //   const {data:{userslist,total,code}} = res;
+          //   console.log(userslist.row);
         },
         methods:{
             async getuserList () {
@@ -129,8 +207,75 @@
                     this.tableData = userslist
                     this.total = total
                 }
+            },
+            handleSizeChange(val) {
+                console.log(`每页 ${val} 条`);
+            },
+            handleCurrentChange(val) {
+                console.log(`当前页: ${val}`);
+            },
+            searchUser(){
+                console.log(1);
+                // this.getuserList()
+            },
+            delect(){
+                this.$message.success('清除成功');
+            },
+            showAddUser:function () {
+                this.form = {}
+                this.dialogFormVisible = true
+            },
+            // 添加用户发送ajax
+             addUser () {
+                this.dialogFormVisible = false
+                this.$http.post(`api`,this.form)
+                // const {data} = res.data;
+                const code = 200;
+                if(code == 200){
+                    // 提示成功
+                    this.$message.success('保存成功')
+                    // 重新请求表格数据
+                    this.getuserList()
+                    // 清空文本框
+                    this.form = {}
+                }
+            },
+            //删除用户
+            delectuser:function (userid) {
+                console.log(userid);
+                this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    //发送删除请求 :id
+                    // 1.
+                    this.$message({
+                        type: 'success',
+                        message: '删除成功!'
+                    });
+                }).catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '已取消删除'
+                    });
+                });
+            },
+            // 编辑用户
+            edituser:function (user) {
+                console.log(user);
+                this.form = user
+                this.edit = true
+            },
+            // 发送编辑用户提交
+            editadduser:function () {
 
+            },
+            // 修改用户状态发送请求
+            changeMgState:function (user) {
+                console.log(user.value1);
             }
+
         }
     }
 </script>
